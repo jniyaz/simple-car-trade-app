a4d7727ec7efdc6d6b8f9c2e90e9f500a2757569
Niyaz <niyaz.jamal@icarasia.com>
2020-08-23 12:31:07 +0800

    material skeleton seyup and faq page fixed

---
+++ src/pages/faq.tsx
@@ -0,0 +1,42 @@
+import { GetStaticProps } from "next"
+import { openDB } from "../openDB"
+import { FaqModel } from '../../api/Faq';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+interface FaqProps {
+    faq: FaqModel[];
+}
+
+export default function Faq({faq}: FaqProps) {
+    return <div>
+        <div>
+            <Typography><h4>FAQ</h4></Typography>
+            {faq.map(f => 
+                <Accordion key={f.id}>
+                    <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                    <Typography>{f.question}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <Typography>
+                        {f.answer}
+                    </Typography>
+                    </AccordionDetails>
+                </Accordion>
+            )}
+        </div>
+    </div>
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const db = await openDB();
+    const faq = await db.all('SELECT * FROM FAQ ORDER BY createDate DESC');
+    return { props: { faq } };
+}
